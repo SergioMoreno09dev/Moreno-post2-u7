c024b33cc9dc0ceb36ba43d64ffebaf8d3bd01f4
Sergio Andrés Moreno Sánchez <fineas25@gmail.com>
2026-04-19 00:45:32 -0500

    AGregar Archivo README.md con informacion del proyecto y capturas de pantalla de postman

---
+++ src/test/java/com/universidad/producto_web2/ProductoWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.producto_web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
